7a6db47fa270c87252b2ccd01bb43d2d1dfd5a83
Shamkhal <shamkhalhuseynzade26@gmail.com>
2024-02-20 23:28:10 +0300

    First Commit

---
+++ src/app/products/[productId]/page.tsx
@@ -0,0 +1,5 @@
+export default function Product({params} : {
+    params : { productId : string}
+} ){
+    return <h1>Product {params.productId}</h1>
+}
---
+++ src/app/products/page.tsx
@@ -0,0 +1,12 @@
+export default function Products(){
+    return (
+        <>
+            <h1>Products</h1>
+            <p>Our products are the best</p>
+            <h2>Product 1</h2>
+            <h2>Product 2</h2>
+            <h2>Product 3</h2>
+
+        </>
+    );
+}
